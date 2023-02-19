db6096e7131b10a319334d18bd7f48e1169799ae
Saikat Dey <30950554+deysaikat04@users.noreply.github.com>
2023-02-19 22:38:52 +0530

    chore: updated db connection and added rate limiter

--- config/db.js
+++ config/db.js
@@ -1,20 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const db = "mongodb+srv://sd123:sd123@cashflow.szgd0.mongodb.net/expensedb?retryWrites=true&w=majority";
+console.log(process.env.MONGO_DB_CONNECTION_URL);
+
+const db =
+  process.env.NODE_ENV === "development"
+    ? process.env.MONGO_DB_LOCAL
+    : `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`;
 
 const conenctDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        });
-        console.log('MongoDB connected..');
-    } catch (error) {
-        consolr.error(error.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("MongoDB connected..");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = conenctDB;
--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const connectDB = require('./config/db');
-const transaction = require('./routes/transaction');
-const user = require('./routes/userRoute');
-const path = require('path');
-const expressSanitizer = require('express-sanitizer');
+const connectDB = require("./config/db");
+const transaction = require("./routes/transaction");
+const user = require("./routes/userRoute");
+const path = require("path");
+const expressSanitizer = require("express-sanitizer");
+const rateLimit = require("express-rate-limit");
 
-var bodyParser = require('body-parser');
-var cors = require('cors');
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+const {
+  AUTH_RATE_LIMIT_MINS,
+  AUTH_RATE_LIMIT_REQ,
+  DEFAULT_RATE_LIMIT_MINS,
+  DEFAULT_RATE_LIMIT_REQ,
+} = process.env;
 
 app.use(cors());
 
@@ -16,13 +25,28 @@ app.use(expressSanitizer());
 
 connectDB();
 
-app.use(express.static('client/build'));
+app.use(express.static("client/build"));
 
-app.get('/favicon.ico', (req, res) => res.status(204));
+const authLimiter = rateLimit({
+  windowMs: AUTH_RATE_LIMIT_MINS * 60 * 1000,
+  max: AUTH_RATE_LIMIT_REQ, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  message: "Too many requests from this IP, please try again after a few  mins",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
+const defaultLimiter = rateLimit({
+  windowMs: DEFAULT_RATE_LIMIT_MINS * 60 * 1000,
+  max: DEFAULT_RATE_LIMIT_REQ, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  message: "Too many requests from this IP, please try again after a few  mins",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
-app.use('/api/transactions', transaction);
-app.use('/api/auth', user);
+app.get("/favicon.ico", (req, res) => res.status(204));
+
+app.use("/api/transactions", defaultLimiter,  transaction);
+app.use("/api/auth", authLimiter, user);
 
 // app.use('*', (req, res) => {
 //     res.status(404).json({ error: { message: 'Invalid route!' } });
@@ -30,9 +54,8 @@ app.use('/api/auth', user);
 
 //Serve static assets in production
 app.use(function (req, res) {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
